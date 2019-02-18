a06bfcb8c0b0dfab1b51c337619a23f31b9a73e8
Justin Goodyer <justin@goodyer.ca>
2019-02-17 21:58:41 -0500

    Updates

--- precache-manifest.a02790401ea3a5feb95b8a6d828deb26.js
+++ precache-manifest.a02790401ea3a5feb95b8a6d828deb26.js
@@ -4,15 +4,15 @@ self.__precacheManifest = [
     "url": "/static/js/runtime~main.229c360f.js"
   },
   {
-    "revision": "0c527f22b62ff5846859",
-    "url": "/static/js/main.0c527f22.chunk.js"
+    "revision": "1b8ae6d28ad8451f17c3",
+    "url": "/static/js/main.1b8ae6d2.chunk.js"
   },
   {
     "revision": "9ccfd20bc005d065bd3f",
     "url": "/static/js/1.9ccfd20b.chunk.js"
   },
   {
-    "revision": "11a8effd79cdc7ae066c08be7a6ce786",
+    "revision": "5ed3048821f823c848aa96903c3073d2",
     "url": "/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.4866560013855a54cb70f610e0456b5e.js"
+  "/precache-manifest.a02790401ea3a5feb95b8a6d828deb26.js"
 );
 
 workbox.clientsClaim();
